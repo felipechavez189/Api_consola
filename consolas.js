a178ec8debe00454cefa298970250099a325bdbe
Felipe <chavezfelipe189@gmail.com>
2024-11-12 18:13:23 -0300

    consolas actualizado

--- consolas.js
+++ consolas.js
@@ -23,24 +23,24 @@ const consolas = [
     },
     {
         "id": 4,
-        "nombre": "Nintendo Switch",
+        "nombre": "Nintendo Switch Lite",
         "marca": "Nintendo",
-        "fecha_lanzamiento": "2017-03-03",
-        "portada": "https://www.gsmpro.cl/cdn/shop/files/nintendo-switch-standard.jpg?v=1698858446"
+        "fecha_lanzamiento": "2019-09-20",
+        "portada": "https://assets.nintendo.com/image/upload/q_auto:best/f_auto/dpr_2.0/ncom/en_US/products/hardware/nintendo-switch-lite-gray/110672-nintendo-switch-lite-gray-front-1200x675"
     },
     {
         "id": 5,
-        "nombre": "Nintendo Switch",
-        "marca": "Nintendo",
-        "fecha_lanzamiento": "2017-03-03",
-        "portada": "https://www.gsmpro.cl/cdn/shop/files/nintendo-switch-standard.jpg?v=1698858446"
+        "nombre": "Xbox Series S",
+        "marca": "Microsoft",
+        "fecha_lanzamiento": "2020-11-10",
+        "portada": "https://i5.walmartimages.com/asr/49908dab-b83a-491b-8754-f99a74661bed.4dea971f81caa4e7663d46b0cea3b5d3.jpeg"
     },
     {
         "id": 6,
-        "nombre": "Nintendo Switch",
-        "marca": "Nintendo",
-        "fecha_lanzamiento": "2017-03-03",
-        "portada": "https://www.gsmpro.cl/cdn/shop/files/nintendo-switch-standard.jpg?v=1698858446"
+        "nombre": "Playstation 5 Slim",
+        "marca": "Sony",
+        "fecha_lanzamiento": "2023-11-10",
+        "portada": "https://www.abcdin.cl/dw/image/v2/BCPP_PRD/on/demandware.static/-/Sites-master-catalog/default/dwcf35a5ff/images/large/3CC27497608.jpg?sw=1200&sh=1200&sm=fit"
     }
     
 ];
